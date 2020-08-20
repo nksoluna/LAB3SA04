77a33781c1b32a7e98dc26e9e2c4ad429d8d497e
nksoluna <61101110286@psu.ac.th>
2020-08-21 02:30:53 +0700

    connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react' 
+import React, { useState, useEffect } from 'react' 
 import { Text , View , ImageBackground , StyleSheet} from 'react-native' 
 import Forecast from './Forecast'
 
 export default function Weather(props) {
-    const  [ forecastInfo , setforecastInfo ] = useState({
+    const  [ forecastInfo , setForecastInfo ] = useState({
     main: 'main',
     description: 'description',
     temp: 0
     })
-    
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=5ce8fbcd859615235e0c019428995822`)
+        .then((response) => response.json())
+        .then((json) => {
+        setForecastInfo({
+        main: json.weather[0].main,
+        description: json.weather[0].description,
+        temp: json.main.temp
+        });
+        })
+        .catch((error) => {
+        console.warn(error);
+        });
+        }
+        }, [props.zipCode])
+       
     return (      
         <View>
             
